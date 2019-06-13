4deae27252f0e5fdd07b8fc94f335c1cd5c3786f
Apple <1plombier.a.paris@gmail.com>
2019-06-12 23:08:31 -0400

    just text

--- src/pages/about.js
+++ src/pages/about.js
@@ -2,7 +2,7 @@ import React from "react";
 
 import Layout from "../components/layout";
 import SEO from "../components/seo";
-import dogIllustration from "../images/dog-illustration.svg";
+import dogIllustration from "../images/water.jpg";
 
 function AboutPage() {
   return (
